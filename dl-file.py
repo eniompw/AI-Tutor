8875f94739b31e6ddb5d1fb790180037de3582e7
eniompw <71254710+eniompw@users.noreply.github.com>
2024-06-02 12:08:56 +0100

    Delete dl-file.py

--- dl-file.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-
-url = "https://ocr.org.uk/Images/676742-question-paper-computer-systems.pdf"
-# url = "https://ocr.org.uk/Images/676943-mark-scheme-computer-systems.pdf"
-
-# Download the file and specify the filename
-urllib.request.urlretrieve(url, "h446-01-qp.pdf")
-
-print("File downloaded")
