4ab70060e374b256b85deccb4d85d0cbcda73e0d
eniompw <71254710+eniompw@users.noreply.github.com>
2024-06-03 14:57:19 +0000

    text to db

--- app.py
+++ app.py
@@ -1,36 +1,36 @@
-from flask import Flask, session
-import os 
-import google.generativeai as genai
+from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
-app.secret_key = os.urandom(16)
-genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
-model = genai.GenerativeModel('gemini-1.5-flash')
 
-@app.route('/')
-def home():
-    return 'use /qp and /ms'
-
-@app.route('/qp')
+@app.route('/q')
 def question():
-    f = open("/tmp/qp.txt", "r")
-    qp = f.read()
-    f.close()
-    qptext = "output just the first question from this text that ends with square brackets [] and nothing else"
-    # nextQ = session['question'] + "output the next question from the text and nothing else (use html to present the information nicely)"
-    q = model.generate_content([qptext, qp])
-    session['question'] = q.text
-    return q.text
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+    cur.execute("SELECT question FROM Questions")
+    row = cur.fetchone()
+    con.close()
 
-@app.route('/ms')
+    processed_row = []
+    for field in row:
+        if isinstance(field, str):
+            field = field.replace("\n", "<br>")
+        processed_row.append(field)
+
+    return str(tuple(processed_row))
+
+@app.route('/a')
 def answer():
-    f = open("/tmp/ms.txt", "r")
-    ms = f.read()
-    f.close()
-    mstext = "output the text from the text that begins Mark Scheme that relates to this question stop when the next question starts with brackets () "
-    mstext += session['question']
-    q = model.generate_content([mstext, ms])
-    session['answer'] = q.text
-    print(session['answer'])
-    q = model.generate_content(["remove markdown and format this using html", session['answer']])
-    return q.text
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+    cur.execute("SELECT answer FROM Questions")
+    row = cur.fetchone()
+    con.close()
+
+    processed_row = []
+    for field in row:
+        if isinstance(field, str):
+            field = field.replace("\n", "<br>")
+        processed_row.append(field)
+
+    return str(tuple(processed_row))
---
+++ sql
@@ -0,0 +1,3 @@
+CREATE TABLE 'Questions' ('QID' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'Question' TEXT NOT NULL, 'Answer' TEXT NOT NULL)
+
+INSERT INTO "Questions" ("Question","Answer") VALUES ('test','test')
