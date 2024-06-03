cf434f0db8b8deeb19635a7b9b54f45a90b25991
eniompw <71254710+eniompw@users.noreply.github.com>
2024-06-03 15:04:03 +0000

    input form

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import sqlite3
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html', output="")
 
 @app.route('/q')
 def question():
