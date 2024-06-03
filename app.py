217e8f8f6e84c74c12bd0fd81b98b1967c62da5f
eniompw <71254710+eniompw@users.noreply.github.com>
2024-06-03 18:37:44 +0000

    student feedback

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request
+import google.generativeai as genai
 import sqlite3
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(16)
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 @app.route('/')
 def home():
-    return render_template('index.html', output="")
-
-@app.route('/q')
-def question():
     con = sqlite3.connect('database.db')
     cur = con.cursor()
     cur.execute("SELECT question FROM Questions")
@@ -20,10 +21,11 @@ def question():
         if isinstance(field, str):
             field = field.replace("\n", "<br>")
         processed_row.append(field)
+    
+    session['question'] = str(tuple(processed_row))
+    return render_template('index.html', question=session['question'], answer="")
 
-    return str(tuple(processed_row))
-
-@app.route('/a')
+@app.route('/response')
 def answer():
     con = sqlite3.connect('database.db')
     cur = con.cursor()
@@ -37,4 +39,13 @@ def answer():
             field = field.replace("\n", "<br>")
         processed_row.append(field)
 
-    return str(tuple(processed_row))
+    query = "use this question: \n" 
+    query += session['question']
+    query += "the students answer: \n"
+    query += request.args.get('ans')
+    query += "and the mark scheme: \n"
+    query += str(tuple(processed_row))
+    query += "now give feedback on the students answer."
+    response = model.generate_content(query)
+    response = model.generate_content("remove markdown and format this using html: \n" + response.text)
+    return render_template('index.html', question=session['question'], answer=response.text)
