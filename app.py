dc6f17488bfe927ac8ce1c55044a567bb0a7d709
eniompw <71254710+eniompw@users.noreply.github.com>
2024-06-03 10:46:13 +0000

    v0.1

--- app.py
+++ app.py
@@ -9,16 +9,16 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return 'use /qp and /ms'
 
 @app.route('/qp')
 def question():
     f = open("/tmp/qp.txt", "r")
     qp = f.read()
     f.close()
-    qpinstructions = "output just the first question from this text and nothing else (use html to present the information nicely)"
+    qptext = "output just the first question from this text that ends with square brackets [] and nothing else"
     # nextQ = session['question'] + "output the next question from the text and nothing else (use html to present the information nicely)"
-    q = model.generate_content([qpinstructions, qp])
+    q = model.generate_content([qptext, qp])
     session['question'] = q.text
     return q.text
 
@@ -27,6 +27,10 @@ def answer():
     f = open("/tmp/ms.txt", "r")
     ms = f.read()
     f.close()
-    msinstructions = "output the text from the following document that relates to this question and nothing else (use html to present the information nicely)" + session['question']
-    q = model.generate_content([msinstructions, ms])
+    mstext = "output the text from the text that begins Mark Scheme that relates to this question stop when the next question starts with brackets () "
+    mstext += session['question']
+    q = model.generate_content([mstext, ms])
+    session['answer'] = q.text
+    print(session['answer'])
+    q = model.generate_content(["remove markdown and format this using html", session['answer']])
     return q.text
