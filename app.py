823a34af7ebb5e08109c265fa861ebe1f803912c
eniompw <71254710+eniompw@users.noreply.github.com>
2024-06-05 14:56:36 +0000

    api toggle

--- app.py
+++ app.py
@@ -9,32 +9,20 @@ import google.generativeai as genai
 genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 model = genai.GenerativeModel('gemini-1.5-flash')
 
-# from groq import Groq
-# client = Groq(api_key=os.environ.get("GROQ_API_KEY"),)
+from groq import Groq
+client = Groq(api_key=os.environ.get("GROQ_API_KEY"),)
 
-# def groqAI(q):
-#     chat_completion = client.chat.completions.create(
-#     messages=[
-#         {   
-#             "role": "user",
-#             "content": str(q),
-#         }
-#     ],
-#     model="llama3-8b-8192",
-#     )
-#     return chat_completion.choices[0].message.content
-
-@app.route('/previous')
-def previous():
-    if session['number'] > 0:
-        session['number'] -= 1
-    return redirect('/')
-
-@app.route('/next')
-def next():
-    if session['number'] < session['total']:
-        session['number'] += 1
-    return redirect('/')
+def groqAI(q):
+    chat_completion = client.chat.completions.create(
+    messages=[
+        {   
+            "role": "user",
+            "content": str(q),
+        }
+    ],
+    model="llama3-70b-8192",
+    )
+    return chat_completion.choices[0].message.content
 
 @app.route('/')
 def home():
@@ -48,8 +36,20 @@ def home():
     session['total'] = len(row)
     query = "clean this up and replace it with well presented html "
     query += "only show the question in your response dont add anything else: "
-    # session['question'] = groqAI(query + n2br(row[session['number']][0]))
-    session['question'] = model.generate_content(query + n2br(row[session['number']][0])).text
+    file_path = "/tmp/api.txt"
+    if os.path.exists(file_path):
+        f = open(file_path, "r")
+        api = f.read()
+        f.close()
+        session['api'] = api
+    else:
+        session['api'] = "gemini"
+    if session['api'] == "gemini":
+        session['question'] = model.generate_content(query + n2br(row[session['number']][0])).text
+    elif session['api'] == "llama":
+        session['question'] = groqAI(query + n2br(row[session['number']][0]))
+    else:
+        return "select api"
     return render_template('index.html', question=session['question'], answer="")
 
 @app.route('/response')
@@ -68,13 +68,16 @@ def answer():
     query += n2br(row[session['number']][0])
     query += "\n now give feedback on the students answer."
 
-    response = model.generate_content(query)
-    response = model.generate_content("remove markdown and format this using html: \n" + response.text)
-    return render_template('index.html', question=session['question'], answer=response.text)
-
-    # response = groqAI(query)
-    # response = groqAI("remove markdown and format this using html. (don't mention the formatting of your response): \n" + response)
-    # return render_template('index.html', question=session['question'], answer=response)
+    if session['api'] == "gemini":
+        response = model.generate_content(query)
+        response = model.generate_content("remove markdown and format this using html: \n" + response.text).text
+    elif session['api'] == "llama":
+        response = groqAI(query)
+        response = groqAI("remove markdown and format this using html. (don't mention the formatting of your response): \n" + response)
+    else:
+        return "select api"
+    
+    return render_template('index.html', question=session['question'], answer=response)
 
 def n2br(row):
     processed_row = []
@@ -83,3 +86,40 @@ def n2br(row):
             field = field.replace("\n", "<br>")
         processed_row.append(field)
     return str(tuple(processed_row))
+
+@app.route('/api')
+def api():
+    file_path = "/tmp/api.txt"
+    if os.path.exists(file_path):
+        f = open(file_path, "r")
+        api = f.read()
+        f.close()
+        return api
+    else:
+        return "gemini as default"
+
+@app.route('/gemini')
+def gemini():
+    f = open("/tmp/api.txt", "w")
+    f.write("gemini")
+    f.close()
+    return 'gemini'
+
+@app.route('/llama')
+def llama():
+    f = open("/tmp/api.txt", "w")
+    f.write("llama")
+    f.close()
+    return 'llama'
+
+@app.route('/previous')
+def previous():
+    if session['number'] > 0:
+        session['number'] -= 1
+    return redirect('/')
+
+@app.route('/next')
+def next():
+    if session['number'] < session['total']:
+        session['number'] += 1
+    return redirect('/')
