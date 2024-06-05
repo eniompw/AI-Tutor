bf1ec39238e68ad670a20f015fdee73bc32dcd6e
eniompw <71254710+eniompw@users.noreply.github.com>
2024-06-05 14:41:55 +0000

    gemini

--- app.py
+++ app.py
@@ -5,24 +5,24 @@ import os
 app = Flask(__name__)
 app.secret_key = os.urandom(16)
 
-# import google.generativeai as genai
-# genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
-# model = genai.GenerativeModel('gemini-1.5-flash')
+import google.generativeai as genai
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+model = genai.GenerativeModel('gemini-1.5-flash')
 
-from groq import Groq
-client = Groq(api_key=os.environ.get("GROQ_API_KEY"),)
+# from groq import Groq
+# client = Groq(api_key=os.environ.get("GROQ_API_KEY"),)
 
-def groqAI(q):
-    chat_completion = client.chat.completions.create(
-    messages=[
-        {   
-            "role": "user",
-            "content": str(q),
-        }
-    ],
-    model="llama3-8b-8192",
-    )
-    return chat_completion.choices[0].message.content
+# def groqAI(q):
+#     chat_completion = client.chat.completions.create(
+#     messages=[
+#         {   
+#             "role": "user",
+#             "content": str(q),
+#         }
+#     ],
+#     model="llama3-8b-8192",
+#     )
+#     return chat_completion.choices[0].message.content
 
 @app.route('/previous')
 def previous():
@@ -48,8 +48,8 @@ def home():
     session['total'] = len(row)
     query = "clean this up and replace it with well presented html "
     query += "only show the question in your response dont add anything else: "
-    session['question'] = groqAI(query + n2br(row[session['number']][0]))
-    # session['question'] = model.generate_content(query + n2br(row[session['number']][0]))
+    # session['question'] = groqAI(query + n2br(row[session['number']][0]))
+    session['question'] = model.generate_content(query + n2br(row[session['number']][0])).text
     return render_template('index.html', question=session['question'], answer="")
 
 @app.route('/response')
@@ -68,13 +68,13 @@ def answer():
     query += n2br(row[session['number']][0])
     query += "\n now give feedback on the students answer."
 
-    # response = model.generate_content(query)
-    # response = model.generate_content("remove markdown and format this using html: \n" + response.text)
-    # return render_template('index.html', question=session['question'], answer=response.text)
+    response = model.generate_content(query)
+    response = model.generate_content("remove markdown and format this using html: \n" + response.text)
+    return render_template('index.html', question=session['question'], answer=response.text)
 
-    response = groqAI(query)
-    response = groqAI("remove markdown and format this using html. (don't mention the formatting of your response): \n" + response)
-    return render_template('index.html', question=session['question'], answer=response)
+    # response = groqAI(query)
+    # response = groqAI("remove markdown and format this using html. (don't mention the formatting of your response): \n" + response)
+    # return render_template('index.html', question=session['question'], answer=response)
 
 def n2br(row):
     processed_row = []
